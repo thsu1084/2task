d3a97593d6b2a603471e5a144a459ea80ea3da1c
thsu1084 <thsu1084@naver.com>
2023-05-28 13:00:18 +0900

    commit

---
+++ src/test/java/zerobase/weather/JpaMemoRepositoryTest.java
@@ -0,0 +1,46 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+
+    @Test
+    void insertMemoTest(){
+        Memo newMemo = new Memo(10,"this is jpa memo");
+
+        jpaMemoRepository.save(newMemo);
+
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+    }
+
+    @Test
+    void findByIdTest(){
+        Memo newMemo = new Memo(11,"jpa");
+
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+
+        assertEquals(result.get().getText(),"jpa");
+    }
+
+}
